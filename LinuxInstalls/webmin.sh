b3ddd58d88565051542dbf0094aef5d1af55845b
mnwalker <mark@awcode.com>
2016-08-02 22:33:58 +0700

    First webmin installer

---
+++ LinuxInstalls/webmin.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# /* Created by Mark Walker 2016
+# * Run using command "sudo webmin.sh"
+# * For debian/ubuntu based distros only
+# * Licensed under MIT
+# */
+
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+
+cd /root
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+rm jcameron-key.asc
+
+apt-get update
+apt-get install -y webmin
